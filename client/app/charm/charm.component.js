f831faf6df2db67d83ae7c2438432422a2ce5c8d
raju-dev <rajuthapa.1012@gmail.com>
2018-09-07 00:35:27 +0530

    Changes for charm component.

--- client/app/charm/charm.component.js
+++ client/app/charm/charm.component.js
@@ -8,9 +8,9 @@ class CharmComponent {
     this.fields = [
       {field: 'stone_type', heading: 'Stone Type', dataType: 'text'},
       {field: 'color', title: 'Color', dataType: 'text'},
-      {field: 'diamond_type', dtitle: 'Diamond Type', dataType: 'text'},
-      {field: 'stone_c_weight', dtitle: 'Diamond Type', dataType: 'number'},
-      {field: 'stone_k_weight', dtitle: 'Diamond Type', dataType: 'number'},
+      {field: 'diamond_type', title: 'Diamond Type', dataType: 'text'},
+      {field: 'stone_c_weight', title: 'Diamond Type', dataType: 'number'},
+      {field: 'stone_k_weight', title: 'Diamond Type', dataType: 'number'},
     ];
   }
 }
--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -28,14 +28,14 @@ exports = module.exports = { // Can not put all settings here as this needs a re
   },
   menu: {
     pages: [ // Main menu. The `role: manager` is only to show/hide it from the menu, this is not for authentication purpose, the user access level is controlled from the router page
-      { text: 'Books', icon: 'book', url: 'book', authenticate: true },
+      // { text: 'Books', icon: 'book', url: 'book', authenticate: true },
       { text: 'Charms', icon: 'charm', url: 'charm', authenticate: true },
-      { text: 'Movies', icon: 'movie', url: 'movie', authenticate: true },
-      { text: 'Contacts', icon: 'contacts', url: 'contact', authenticate: true },
-      { text: 'Customers', icon: 'people', url: 'customer', authenticate: true },
-      { text: 'Tasks', icon: 'assignment', url: 'task', authenticate: true },
-      { text: 'Documentation', icon: 'description', url: 'doc.pre' },
-      { text: 'Media Library', icon: 'photo_library', url: 'media', authenticate: true }
+      // { text: 'Movies', icon: 'movie', url: 'movie', authenticate: true },
+      // { text: 'Contacts', icon: 'contacts', url: 'contact', authenticate: true },
+      // { text: 'Customers', icon: 'people', url: 'customer', authenticate: true },
+      // { text: 'Tasks', icon: 'assignment', url: 'task', authenticate: true },
+      // { text: 'Documentation', icon: 'description', url: 'doc.pre' },
+      // { text: 'Media Library', icon: 'photo_library', url: 'media', authenticate: true }
     ],
     user: [ // Separate panel for user management tasks for both admin and user
       { text: 'Users', url: 'admin', authenticate: true, role: 'admin', icon: 'perm_identity' },

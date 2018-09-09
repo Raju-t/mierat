0086e98d0c4136613114ac0faf905b6689327d79
Raju-t <rajuthapa.10@gmail.com>
2018-09-09 14:33:41 +0530

    Changes for user controller.

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -174,12 +174,13 @@ export function changePassword(req, res) {
  */
 export function me(req, res, next) {
   var userId = req.user._id;
-
-  return User.findOne({ _id: userId }, '-salt -password')
+  
+  return User.findOne({ where: {_id: userId }}, '-salt -password')
     .then(user => { // don't ever give out the password or salt
       if (!user) {
         return res.status(401).end();
       }
+      console.log(user.toJSON());
       res.json(user);
     })
     .catch(err => next(err));

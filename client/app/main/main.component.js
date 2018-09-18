7971d6b69af3eb3bd2acc3378c1640881a463444
Raju-t <rajuthapa.10@gmail.com>
2018-09-19 01:04:50 +0530

    Changes for main.

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -12,17 +12,27 @@ export class MainComponent {
     this.pages = [];
     this.pages = appConfig.menu.pages;
     this.$http = $http;
-    Auth.getCurrentUserSync().$promise.then((user) => {
-     this.user = user; 
-     if(this.user.profile.role == 'user' || !this.user || !this.user.profile){
+    if(Auth.getCurrentUserSync().$promise){
+      Auth.getCurrentUserSync().$promise.then((user) => {
+      this.user = user; 
+      if(this.user.profile.role == 'user' || !this.user || !this.user.profile){
       vm.$http.get('/api/media/public_images').then(function (res) {
         vm.loading = false;
         vm.data = res.data;
       }, function (err) {
         vm.handleError(err, vm)
       });
-     }
-   });
+      }
+     });  
+    } else {
+      vm.$http.get('/api/media/public_images').then(function (res) {
+        vm.loading = false;
+        vm.data = res.data;
+      }, function (err) {
+        vm.handleError(err, vm)
+      });
+    }
+    
   }
   getColor($index) {
     var _d = ($index + 1) % 11;
@@ -50,7 +60,7 @@ export default angular.module('materialCrudSqlApp.main', [uiRouter])
   .config(routes)
   .run(function($rootScope, $cookies, $location, $state){
     $rootScope.$on('$locationChangeStart', function (event, next, current) {
-      if(!$cookies.get('token')){
+      if(!$cookies.get('token') && next!=window.location.origin+"/"){
         $location.path('/login');
       }
     });
